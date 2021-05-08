6b47d4281934bfea2917526e34c9e59d8da66014
Inggo E <inggo.espinosa@gmail.com>
2021-05-08 21:53:33 +0800

    Add installer/updater
    Add installer/updater

---
+++ mb-filmfest.php
@@ -0,0 +1,20 @@
+﻿<?php
+/**
+ * Mateship and Bayanihan Film Festival
+ * 
+ * @author          Inggo Espinosa
+ * @copyright       2021 Inggo.dev
+ * @license         GPLv3
+ * 
+ * @wordpress-plugin
+ * Plugin Name:     Mateship and Bayanihan Film Festival
+ * Version:         0.1.0
+ * Author URI:      https://inggo.dev
+ */
+namespace Inggo\WordPress\MBFilmFest;
+
+include_once(\plugin_dir_path(__FILE__) . 'Updater.php');
+
+$updater = new Updater(__FILE__);
+$updater->setUsername('inggo');
+$updater->setRepository('wp-mbfilmfest');

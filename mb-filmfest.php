a96b29c80167e85cd04a30d8cec7545b3e633d84
Inggo E <inggo.espinosa@gmail.com>
2021-05-08 22:34:40 +0800

    Update encoding

--- mb-filmfest.php
+++ mb-filmfest.php
@@ -10,7 +10,7 @@ namespace Inggo\WordPress\MBFilmFest;
  * 
  * @wordpress-plugin
  * Plugin Name:     Mateship and Bayanihan Film Festival
- * Version:         0.1.5
+ * Version:         0.1.6
  * Author:          Inggo Espinosa
  * Author URI:      https://inggo.dev
  * License:         GPLv3
@@ -23,7 +23,10 @@ spl_autoload_register(__NAMESPACE__ . "\autoloader");
 function autoloader($class_name)
 {
     $classes_dir = realpath(\plugin_dir_path(__FILE__ )) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR;
-    $class_file = str_replace('_', DIRECTORY_SEPARATOR, $class_name) . '.php';
+    // Remove namespace from directory file
+    $class_file = str_replace('', __NAMESPACE__, $class_name);
+    // Replace all directory separators and append '.php' extension
+    $class_file = str_replace('\\', DIRECTORY_SEPARATOR, $class_file) . '.php';
     require_once $classes_dir . $class_file;
 }
 

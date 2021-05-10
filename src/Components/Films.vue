6b68618e9a37ad5c4a83f31c800b883ed954ac04
Inggo E <inggo.espinosa@gmail.com>
2021-05-10 12:43:08 +0800

    Minor updates

--- src/Components/Films.vue
+++ src/Components/Films.vue
@@ -23,7 +23,7 @@
         </div>
         <h4>{{ film.title }}</h4>
         <div class="mbfilmfest_description_container">
-          <p v-html="film.description.replace('\r','').replace('\n','<br>')"></p>
+          <p v-html="film.description.replace(/\r/g,'').replace(/\n/g,'<br>')"></p>
         </div>
       </li>
     </ul>
